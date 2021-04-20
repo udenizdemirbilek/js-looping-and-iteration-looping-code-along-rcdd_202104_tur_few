6f570fdc9fd002a5700f94ec82195604afb54e63
demirbileku <udemirbilek2@gmail.com>
2021-04-20 13:53:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    let arr = [];
+for (let i = 0; i < array.length; i++){
+    arr[i] = "Thank you, " + array[i] + ", for the wonderful " + string + " gift!";
+}
+return arr;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
